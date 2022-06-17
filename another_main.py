5cfb8cf5dad3b50bcb97935e5e257249b109bd07
user <hochu-leto@yandex.ru>
2022-06-17 19:27:25 +0300

    проверить на машине

--- another_main.py
+++ another_main.py
@@ -1,25 +1,11 @@
 from canlib import canlib, Frame
-from canlib.canlib import ChannelData
 
 
 def setUpChannel(channel=0,
                  openFlags=canlib.Open.ACCEPT_VIRTUAL,
                  outputControl=canlib.Driver.NORMAL):
     ch = canlib.openChannel(channel, bitrate=canlib.Bitrate.BITRATE_125K, flags=openFlags)
-    print("Using channel: %s, EAN: %s" % (ChannelData(channel).channel_name,
-                                          ChannelData(channel).card_upc_no))
     ch.setBusOutputControl(outputControl)
-    # Specifying a bus speed of 250 kbit/s. See documentation
-    # # for more informationon how to set bus parameters.
-    # params = canlib.busparams.BusParamsTq(
-    #     tq=8,
-    #     phase1=2,
-    #     phase2=2,
-    #     sjw=1,
-    #     prescaler=40,
-    #     prop=3
-    # )
-    # ch.set_bus_params_tq(params)
     ch.busOn()
     return ch
 
@@ -29,15 +15,14 @@ def tearDownChannel(ch):
     ch.close()
 
 
-print("canlib version:", canlib.dllversion())
 ch0 = setUpChannel(channel=0)
-ch1 = setUpChannel(channel=1)
 # frame = Frame(
 #     id_=100,
 #     data=[1, 2, 3, 4],
 #     flags=canlib.MessageFlag.EXT
 # )
 # ch1.write(frame)
+
 while True:
     try:
         frame = ch0.read()
@@ -45,10 +30,9 @@ while True:
         for i in frame.data:
             print(hex(i), end=' ')
         print()
-        # break
     except canlib.canNoMsg:
-        pass
+        print('No CAN')
+        tearDownChannel(ch0)
     except canlib.canError as ex:
         print(ex)
-tearDownChannel(ch0)
-tearDownChannel(ch1)
+        tearDownChannel(ch0)
