e8e832e685c06d5ad2501f2d982b11608c259d58
user <hochu-leto@yandex.ru>
2022-06-21 10:57:27 +0300

    не работает

--- another_main.py
+++ another_main.py
@@ -4,7 +4,12 @@ from canlib import canlib, Frame
 def setUpChannel(channel=0,
                  openFlags=canlib.Open.ACCEPT_VIRTUAL,
                  outputControl=canlib.Driver.NORMAL):
-    ch = canlib.openChannel(channel, bitrate=canlib.Bitrate.BITRATE_125K, flags=openFlags)
+    try:
+        ch = canlib.openChannel(channel, bitrate=canlib.Bitrate.BITRATE_125K, flags=openFlags)
+    except canlib.canError as ex:
+        print(ex)
+        return False
+
     ch.setBusOutputControl(outputControl)
     ch.busOn()
     return ch
@@ -15,24 +20,24 @@ def tearDownChannel(ch):
     ch.close()
 
 
-ch0 = setUpChannel(channel=0)
+ch0 = setUpChannel(channel=1)
 # frame = Frame(
 #     id_=100,
 #     data=[1, 2, 3, 4],
 #     flags=canlib.MessageFlag.EXT
 # )
 # ch1.write(frame)
-
-while True:
-    try:
-        frame = ch0.read()
-        print(hex(frame.id), end='    ')
-        for i in frame.data:
-            print(hex(i), end=' ')
-        print()
-    except canlib.canNoMsg:
-        print('No CAN')
-        tearDownChannel(ch0)
-    except canlib.canError as ex:
-        print(ex)
-        tearDownChannel(ch0)
+if ch0:
+    while True:
+        try:
+            frame = ch0.read()
+            print(hex(frame.id), end='    ')
+            for i in frame.data:
+                print(hex(i), end=' ')
+            print()
+        except canlib.canNoMsg:
+            print('No CAN')
+            tearDownChannel(ch0)
+        except canlib.canError as ex:
+            print(ex)
+            tearDownChannel(ch0)
